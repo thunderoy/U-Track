e4eb2aedeff8680341a2bdc7dada1ebe95b213a7
thunderoy <thunderoy013@gmail.com>
2020-08-13 14:09:16 +0530

    updated settings.py file

--- utrack/settings.py
+++ utrack/settings.py
@@ -129,5 +129,3 @@ STATIC_URL = '/static/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
